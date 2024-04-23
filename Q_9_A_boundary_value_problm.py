f05b30cce1dee6cb0e2551859bda83d1e6cafa79
subhas manna <subhasmanna875@gmail.com>
2024-04-23 23:59:01 +0530

    Q_9_A_BVP_Usingn scipy

---
+++ Q_9_A_boundary_value_problm.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy.integrate import solve_bvp
+import matplotlib.pyplot as plt
+
+def ode(x, y):
+    y1, y2 = y
+    dydx = [y2, -np.exp(-2*y1)]
+    return dydx
+
+def bc(ya, yb):
+    return np.array([ya[0] - 0, yb[0] - np.log(2)])
+
+# Initial guess for the solution
+x = np.linspace(1, 2, 100)
+y_guess = np.zeros((2, x.size))
+
+# Solve the boundary value problem
+sol = solve_bvp(ode, bc, x, y_guess)
+
+# Plot the solution
+plt.plot(sol.x, sol.y[0], label='y(x)')
+'''
+plt.plot(sol.x,sol.y[1],label="y' ")
+'''
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Solution of y\'\' = -e^(-2y)')
+plt.legend()
+plt.grid()
+plt.show()
